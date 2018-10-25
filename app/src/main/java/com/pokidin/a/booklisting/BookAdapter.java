eb467c897936eaafc42373c2b5d24350f6c956b4
AlexanderPokidin <a.pokidin@gmail.com>
2018-10-25 22:32:19 +0300

    BookAdapter added

---
+++ app/src/main/java/com/pokidin/a/booklisting/BookAdapter.java
@@ -0,0 +1,49 @@
+package com.pokidin.a.booklisting;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class BookAdapter extends ArrayAdapter<Book> {
+    public static final String TAG = BookAdapter.class.getSimpleName();
+
+
+
+    public BookAdapter(@NonNull Activity context, ArrayList<Book> books) {
+        super(context, 0, books);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View listItemView = convertView;
+        if (listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+        Book currentBook = getItem(position);
+        TextView titleTextView = listItemView.findViewById(R.id.tv_title);
+        titleTextView.setText(currentBook.getTitle());
+
+        TextView publisherTextView = listItemView.findViewById(R.id.tv_publisher);
+        publisherTextView.setText(currentBook.getPublisher());
+
+        TextView dateNextView = listItemView.findViewById(R.id.tv_date);
+        dateNextView.setText(String.valueOf(currentBook.getPublishedDate()));
+
+        TextView descriptionTextView = listItemView.findViewById(R.id.tv_description);
+        descriptionTextView.setText(currentBook.getDescription());
+
+        TextView authorsTextView = listItemView.findViewById(R.id.tv_authors);
+//        authorsTextView.setText(currentBook.getAuthors());
+
+        return listItemView;
+    }
+}
